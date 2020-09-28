aa2eedab063c4aa1058941e963f1a7c0e53f9657
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2020-09-28 12:06:49 +0700

    fix overflow

--- CitadelManager/resources/views/layouts/components/appuseractivationeditor.blade.php
+++ CitadelManager/resources/views/layouts/components/appuseractivationeditor.blade.php
@@ -30,7 +30,9 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_user_full_name">User Name:</label>
-                                        <input type="text" disabled="disabled" name="editor_activation_input_user_full_name" id="editor_activation_input_user_full_name"
+                                        <input type="text" disabled="disabled"
+                                               name="editor_activation_input_user_full_name"
+                                               id="editor_activation_input_user_full_name"
                                         />
                                     </div>
                                 </div>
@@ -38,7 +40,8 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_identifier">Identifier:</label>
-                                        <input type="text" disabled="disabled" name="editor_activation_input_identifier" id="editor_activation_input_identifier"
+                                        <input type="text" disabled="disabled" name="editor_activation_input_identifier"
+                                               id="editor_activation_input_identifier"
                                         />
                                     </div>
                                 </div>
@@ -46,7 +49,8 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_device_id">Device Id:</label>
-                                        <input type="text" disabled="disabled" name="editor_activation_input_device_id" id="editor_activation_input_device_id" />
+                                        <input type="text" disabled="disabled" name="editor_activation_input_device_id"
+                                               id="editor_activation_input_device_id"/>
                                     </div>
                                 </div>
                             </div>
@@ -56,7 +60,8 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_ip_address">IP Address:</label>
-                                        <input type="text" disabled="disabled" name="editor_activation_input_ip_address" id="editor_activation_input_ip_address"
+                                        <input type="text" disabled="disabled" name="editor_activation_input_ip_address"
+                                               id="editor_activation_input_ip_address"
                                         />
                                     </div>
                                 </div>
@@ -65,10 +70,12 @@
                                     <br/>
                                     <br/>
                                     <label class="switch-original">
-                                        <input type="checkbox" id="editor_activation_report_level" name="editor_activation_report_level">
+                                        <input type="checkbox" id="editor_activation_report_level"
+                                               name="editor_activation_report_level">
                                         <span class="check"></span>
                                     </label>
-                                    <label id="editor_activation_report_level_text" class="report-level-width">No reporting back to server</label>
+                                    <label id="editor_activation_report_level_text" class="report-level-width">No
+                                        reporting back to server</label>
                                 </div>
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
@@ -84,7 +91,8 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_bypass_quantity">Bypass Quantity:</label>
-                                        <input type="number" name="editor_activation_input_bypass_quantity" id="editor_activation_input_bypass_quantity" />
+                                        <input type="number" name="editor_activation_input_bypass_quantity"
+                                               id="editor_activation_input_bypass_quantity"/>
                                         <button class="button helper-button reveal" tabindex="-1" type="button">
                                             <span class="mif-looks"></span>
                                         </button>
@@ -94,7 +102,8 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_bypass_period">Bypass Period (min):</label>
-                                        <input type="number" name="editor_activation_input_bypass_period" id="editor_activation_input_bypass_period" />
+                                        <input type="number" name="editor_activation_input_bypass_period"
+                                               id="editor_activation_input_bypass_period"/>
                                         <button class="button helper-button clear" type="button">
                                             <span class="mif-cross"></span>
                                         </button>
@@ -104,7 +113,9 @@
                                 <div class="cell cell-auto-size">
                                     <div class="input-control text" data-role="input">
                                         <label for="editor_activation_input_bypass_used">Bypass Used:</label>
-                                        <input type="number" disabled="disabled" name="editor_activation_input_bypass_used" id="editor_activation_input_bypass_used"
+                                        <input type="number" disabled="disabled"
+                                               name="editor_activation_input_bypass_used"
+                                               id="editor_activation_input_bypass_used"
                                         />
                                     </div>
                                 </div>
@@ -119,16 +130,19 @@
                                 <h2>
                                     <small>Activation's Blocked Sites</small>
                                 </h2>
-                                <hr class="thin" />
-                                <button type="button" event-click="addNewSelfModerationSite" class="button primary">Add Site</button>
+                                <hr class="thin"/>
+                                <button type="button" event-click="addNewSelfModerationSite" class="button primary">Add
+                                    Site
+                                </button>
 
-                                <table id="activation_self_moderation_table" class="table striped hovered border black-background" style="width: 100%">
+                                <table id="activation_self_moderation_table"
+                                       class="table striped hovered border black-background" style="width: 100%">
                                     <thead>
-                                        <tr>
-                                            <th>Site</th>
-                                            <th>...</th>
-                                            <th>...</th>
-                                        </tr>
+                                    <tr>
+                                        <th>Site</th>
+                                        <th>...</th>
+                                        <th>...</th>
+                                    </tr>
                                     </thead>
                                     <tbody id="activation_self_moderation_insert">
 
@@ -142,21 +156,25 @@
                                 <h2>
                                     <small>Activation's Allowed Sites</small>
                                 </h2>
-                                <hr class="thin" />
-                                <button type="button" event-click="addNewWhitelistSite" class="button primary">Add Site</button>
-
-                                <table id="activation_whitelist_table" class="table striped hovered border black-background" style="width: 100%">
-                                    <thead>
+                                <hr class="thin"/>
+                                <button type="button" event-click="addNewWhitelistSite" class="button primary">Add
+                                    Site
+                                </button>
+                                <div style="max-height: 350px; overflow-y: auto;">
+                                    <table id="activation_whitelist_table"
+                                           class="table striped hovered border black-background" style="width: 100%">
+                                        <thead>
                                         <tr>
                                             <th>Site</th>
                                             <th>...</th>
                                             <th>...</th>
                                         </tr>
-                                    </thead>
-                                    <tbody id="activation_whitelist_table">
+                                        </thead>
+                                        <tbody id="activation_whitelist_table">
 
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -173,7 +191,7 @@
                         </div>
                     </div>
                 </div>
-                        
+
             </form>
         </div>
     </div>
