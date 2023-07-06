6201f52afdb13d9cd675c36408db77d361a10a9d
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2023-07-06 12:11:14 +0700

    hash calculation fixes

--- CitadelManager/app/FilterRulesManager.php
+++ CitadelManager/app/FilterRulesManager.php
@@ -14,30 +14,35 @@ use Illuminate\Support\Facades\DB;
 use Log;
 use SplFileObject;
 
-class FilterRulesManager {
+class FilterRulesManager
+{
     const TYPES = [
         'Filters' => 'rules',
         'Triggers' => 'triggers'
     ];
 
 
-    public function getRuleDataPath(): string {
+    public function getRuleDataPath(): string
+    {
         $storageDir = resource_path();
         $rulesZipPath = $storageDir . DIRECTORY_SEPARATOR . 'global-rules.zip';
         return $rulesZipPath;
     }
 
-    public function getFilename($listNamespace, $listCategory, $filename, $separatorChar = '.') {
+    public function getFilename($listNamespace, $listCategory, $filename, $separatorChar = '.')
+    {
         return $separatorChar . $listNamespace .
             $separatorChar . $listCategory .
             $separatorChar . $filename;
     }
 
-    public function getEtag($path) {
+    public function getEtag($path)
+    {
         return hash_file('sha1', $path);
     }
 
-    public function getRulesetPath($namespace, $category, $type) {
+    public function getRulesetPath($namespace, $category, $type)
+    {
         $filename = $this->getFilename($namespace, $category, "$type.txt");
 
         $storageDir = resource_path() . DIRECTORY_SEPARATOR . "rules" . DIRECTORY_SEPARATOR . $this->getDirForRuleset($category);
@@ -47,11 +52,13 @@ class FilterRulesManager {
         return $storageDir . $filename;
     }
 
-    private function getDirForRuleset(string $category): string {
+    private function getDirForRuleset(string $category): string
+    {
         return $category[0] . DIRECTORY_SEPARATOR . $category[1] . DIRECTORY_SEPARATOR;
     }
 
-    public function buildRuleData() {
+    public function buildRuleData()
+    {
         Log::debug('Building Rule Data');
 
         foreach (FilterList::cursor() as $list) {
@@ -68,10 +75,11 @@ class FilterRulesManager {
         }
     }
 
-    public function buildRuleset($namespace, $category, $type, $filterList) {
+    public function buildRuleset($namespace, $category, $type, $filterList)
+    {
         $filename = $this->getFilename($namespace, $category, "$type.txt");
 
-        $filePath = $this->buildFile($filename, $category, TextFilteringRule::where('filter_list_id', '=', $filterList->id)->cursor());
+        $filePath = $this->buildFilePath($filename, $category);
 
         $filterList->file_sha1 = $this->getEtag($filePath);
         $filterList->entries_count = $this->getLines($filePath);
@@ -81,28 +89,20 @@ class FilterRulesManager {
     }
 
     // Helps reduce memory usage for rule file building.
-    public function buildFile($filename, $category, $filters) {
+    public function buildFilePath($filename, $category)
+    {
         $storageDir = resource_path() . DIRECTORY_SEPARATOR . "rules" . DIRECTORY_SEPARATOR . $this->getDirForRuleset($category);
         if (!file_exists($storageDir)) {
             mkdir($storageDir, 0777, true);
         }
 
         $filePath = $storageDir . DIRECTORY_SEPARATOR . $filename;
-        Log::debug('Opening File to write filters to it: ' . $filePath);
-        $file = fopen($filePath, 'w');
-
-        foreach ($filters as $filter) {
-            fprintf($file, "%s\n", $filter->rule);
-        }
-
-        fclose($file);
-        Log::debug('Closing File to write filters to it: ' . $filePath);
-
         return $filePath;
     }
 
     // Helps reduce memory usage for rule file building.
-    public function buildFileFromSpl(SplFileObject $inputFile, FilterList $list, $convertToAbp, $appendToEndOfFile=false) {
+    public function buildFileFromSpl(SplFileObject $inputFile, FilterList $list, $convertToAbp, $appendToEndOfFile = false)
+    {
         if ($convertToAbp) {
             $lineFeedFunc = function (string $in): string {
                 return $this->formatStringAsAbpFilter(trim($in));
@@ -143,7 +143,8 @@ class FilterRulesManager {
         return $filePath;
     }
 
-    private function formatStringAsAbpFilter(string $input): string {
+    private function formatStringAsAbpFilter(string $input): string
+    {
         if (strlen($input) <= 0) {
             return $input;
         }
@@ -151,7 +152,8 @@ class FilterRulesManager {
         return '||' . str_replace('/', '^', $input);
     }
 
-    function getLines($file) {
+    function getLines($file)
+    {
         $file = new \SplFileObject($file, 'r');
         $file->setFlags(SplFileObject::READ_AHEAD | SplFileObject::SKIP_EMPTY |
             SplFileObject::DROP_NEW_LINE);
@@ -160,7 +162,8 @@ class FilterRulesManager {
         return $file->key();
     }
 
-    public function deleteFiles(FilterList $list) {
+    public function deleteFiles(FilterList $list)
+    {
         $listNamespace = $list->namespace;
         $listCategory = $list->category;
         $listType = $list->type;
