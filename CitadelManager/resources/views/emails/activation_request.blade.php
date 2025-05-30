a16fd1626842646793d9331e6178b441615efea9
Brian Penner <brian@planetnine.tech>
2025-05-29 21:16:49 -0400

    clarify activation email

--- CitadelManager/resources/views/emails/activation_request.blade.php
+++ CitadelManager/resources/views/emails/activation_request.blade.php
@@ -1,6 +1,6 @@
 <strong>Hello,</strong>
 <p>This is a notification from {{ config('app.company_name') }}. We have received an activation request from you for your CloudVeil for Windows or CloudVeil for Mac device.</p>
-<p>Please open <a href="{{ $link }}">this link</a> and your app will automatically log in shortly.</p>
+<p>Please open <a href="{{ $link }}">this link</a> and the app will automatically log in shortly.</p>
 
 <p>Best Regards</p>
 <p><strong>{{ config('app.company_name') }} Support</strong></p>
