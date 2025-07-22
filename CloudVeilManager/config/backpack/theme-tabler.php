83602206764d2e4fbbe81041143195aa55e1ac29
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2025-07-22 13:16:50 +0700

    Changed layout to vertical

--- CloudVeilManager/config/backpack/theme-tabler.php
+++ CloudVeilManager/config/backpack/theme-tabler.php
@@ -22,7 +22,7 @@ return [
      * Possible values: horizontal, horizontal_dark, horizontal_overlap, vertical,
      * vertical_dark, vertical_transparent (legacy theme), right_vertical, right_vertical_dark, right_vertical_transparent
      */
-    'layout' => 'horizontal_overlap',
+    'layout' => 'vertical',
 
     /**
      * Pick a login page layout.
